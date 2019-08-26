8f1dae5a27dc25cb9d053d194e15891d668b88ac
lilin <Glilin1576378853@163.com>
2019-08-26 11:14:47 +0800

    创建路由的步骤

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
    <p>这是根组件</p>
+   <!-- 9.添加超链接 -->
+    <router-link to="/index">首页</router-link>&nbsp;&nbsp;
+    <router-link to="/login">登录</router-link>
+    <!-- 8.挖坑router-view -->
+   <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
+/* eslint-disable eol-last */
 import Vue from 'vue'
 import App from './App.vue'
-
+// 1.引入组件 @代表src目录下的文件
+import index from '@/views/index.vue'
+import login from '@/views/login.vue'
+// 2.引入路由
+import VueRouter from 'vue-router'
+// 3.使用路由
+Vue.use(VueRouter)
+    // 4.创建路由对象
+var router = new VueRouter({
+    // 5.配置路由
+    routes: [
+        // 6.配置路由单击对象：路由映射组件
+        {
+            name: 'index',
+            path: '/index',
+            component: index
+        },
+        {
+            name: 'login',
+            path: '/login',
+            component: login
+        }
+    ]
+})
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+    // 7.注入路由，让整个应用都有路由功能
+    router,
+    render: h => h(App)
 }).$mount('#app')
