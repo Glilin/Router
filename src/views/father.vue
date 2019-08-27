8e56b8cbab751bfcac6419c1b3c779dc58e5db2f
lilin <Glilin1576378853@163.com>
2019-08-27 17:38:42 +0800

    创建子组件在父组件中使用

--- src/views/father.vue
+++ src/views/father.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="father">
     <p>这是父组件123123</p>
+    <!-- 3.使用组件就像使用标签 -->
+    <son></son>
   </div>
 </template>
 
 <script>
+// 1.引入子组件
+import son from '@/views/son.vue'
 export default {
+  // 2 组件components注册
+  components: {
+    son
+  }
 }
 </script>
 
---
+++ src/views/son.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="son">
+   <p>这是子组件</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="">
+
+</style>
